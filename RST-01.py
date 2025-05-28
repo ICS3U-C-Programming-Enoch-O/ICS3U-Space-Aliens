a9f21e67a68dc0cd2225c75fb46ec259b60e56ad
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-05-28 17:08:59 +0000

    RST-02

--- RST-01.py
+++ RST-01.py
@@ -1,6 +1,2 @@
-# /bin/env python3
-# Created by: Enoch O
-# Created on: May 14, 2025
-# This program is the "Space-Aliens" Program on the PyBadge
-
 print("Hello, Enoch!")
+print("Hello, welcome to the shadow Domain!")
